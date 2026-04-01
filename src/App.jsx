559f2bf65abf91051daaf278b56417897975b60d
Chan <waanchee@gmail.com>
2026-04-01 13:48:42 +0800

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 //import Navbar from "#components/Navbar.jsx";
 //import Welcome from "#components/Welcome.jsx";
-import { Navbar, Welcome } from "#components";
+import { Dock, Navbar, Welcome } from "#components";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
+      <Dock />
     </main>
   );
 };
